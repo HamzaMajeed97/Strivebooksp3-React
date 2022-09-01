595fe5b1b0aa72451bffb56dabfd7ea660f3c2c1
HamzaMajeed97 <hamzamajeed784@gmail.com>
2022-09-01 22:44:43 +0200

    modifiche

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -2,19 +2,30 @@ import { Component } from "react";
 
 class CommentArea extends Component{
     
+state= {
+comments : []
+}
 
 
 
 
-
-recuperaDati = async (e)  => {
-    e.preventDefault()
+componentDidMount = async ()  => {
     try {
-        let response = await fetch('https://striveschool-api.herokuapp.com/api/comments')
+        let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin
+        ,{
+            headers: {
+                Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwOGU4MWVkZDY3ODAwMTUwN2Q3MzQiLCJpYXQiOjE2NjIwMjk0NDEsImV4cCI6MTY2MzIzOTA0MX0.V3BZhbimXxKJnE2_CHALYYWFMGvvzYXBcX7sjT60dK4'
+            },
+            
+        }
+        )
 
 if (response.ok){
     let data = await response.json()
     console.log(data)
+}else {
+
 }
 
 
